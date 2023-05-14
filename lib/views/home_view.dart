148593998c1f79a4a98e923b3aa724bbc7c766e1
Билалдин <rahmanalievbilaldin@gmail.com>
2023-05-14 08:39:17 +0600

    git

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:developer';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
@@ -37,16 +39,26 @@ class _HomeViewState extends State<HomeView> {
     await abaYraiynAlipKel();
     log('Position latitude ===> ${position.latitude}');
     log('Position longitude ===> ${position.longitude}');
+    getCurrentweather();
+  }
+
+  final dio = Dio();
+  Future<void> getCurrentweather() async {
+    final joop = await dio.get(AppiAdres);
+    final jsonData = json.decode(joop.data);
+    oblast = jsonData('name');
+    log('joooop====> ${oblast}');
   }
 
   String temperatura = '15';
   String oblast = 'osh';
-
+  String AppiAdres =
+      'api.openweathermap.org/data/2.5/weather?lat=37.421998333333335&lon= -122.084&appid=${AppText.myApiKey}';
   abaYraiynAlipKel() async {
     var client = http.Client();
 
     Uri uri = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=37.421998333333335&lon= -122.084&appid=${AppText.myApiKey}');
+        'api.openweathermap.org/data/2.5/weather?lat=37.421998333333335&lon= -122.084&appid=${AppText.myApiKey}');
     final dannyiJoop = await client.get(uri);
     log('dannyi joop ===> $dannyiJoop');
   }
@@ -58,9 +70,6 @@ class _HomeViewState extends State<HomeView> {
     // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
       return Future.error('Location services are disabled.');
     }
 
@@ -68,11 +77,6 @@ class _HomeViewState extends State<HomeView> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
         return Future.error('Location permissions are denied');
       }
     }
@@ -83,8 +87,6 @@ class _HomeViewState extends State<HomeView> {
           'Location permissions are permanently denied, we cannot request permissions.');
     }
 
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
     return await Geolocator.getCurrentPosition();
   }
 
